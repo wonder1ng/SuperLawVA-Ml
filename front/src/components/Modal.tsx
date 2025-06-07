c3b731697fa1dc9c27a8b1f501b46ed956487385
wonder1ng <wonder1ng@velog.io>
2025-06-08 00:27:01 +0900

    refactor: modify Modal component style for easy custom

--- front/src/components/Modal.tsx
+++ front/src/components/Modal.tsx
@@ -5,8 +5,9 @@ import React, { useEffect } from "react";
 interface Props {
   isOpen: boolean;
   setIsOpen: (value: boolean) => void;
-  children: React.ReactNode;
   isCenter?: boolean;
+  height?: number | string;
+  children: React.ReactNode;
   clickOutsideClose?: boolean;
 }
 
@@ -14,8 +15,9 @@ const Modal = ({
   isOpen,
   setIsOpen,
   isCenter = false,
-  clickOutsideClose = true,
+  height,
   children,
+  clickOutsideClose = true,
 }: Props) => {
   useEffect(() => {
     document.body.style.overflow = isOpen ? "hidden" : "auto";
@@ -28,21 +30,27 @@ const Modal = ({
 
   return (
     <div
-      onClick={() => clickOutsideClose && setIsOpen(false)}
-      className={`fixed top-0 left-0 w-screen h-screen bg-black opacity-50 z-50 flex ${
-        isCenter ? "items-center justify-center px-8" : "items-end"
+      className={`fixed top-0 left-0 w-screen h-screen z-50 flex justify-center ${
+        isCenter ? "items-center px-8" : "items-end"
       }`}
     >
+      {/* 🔵 배경에만 opacity 적용 */}
+      <div
+        className="absolute top-0 left-0 w-screen h-screen bg-black opacity-50"
+        onClick={() => clickOutsideClose && setIsOpen(false)}
+      />
+
+      {/* ⚪ 모달 내용은 불투명하게 */}
       <div
         onClick={(e) => e.stopPropagation()}
         className={`
-          bg-white transition-transform duration-300 ease-in-out transform animate-slide-up
-          w-full ${isCenter ? "max-w-[45rem] rounded-xl" : "rounded-t-xl"}
-          max-h-[80%] ${
-            isCenter ? "min-h-0" : "min-h-[50%]"
-          } p-8 shadow-[0_-2px_10px_rgba(0,0,0,0.2)]
-          flex flex-col items-center gap-2
-        `}
+        relative z-50 bg-white transition-transform duration-300 ease-in-out transform animate-slide-up
+        w-full max-w-[765px] ${isCenter ? "rounded-[50px]" : "rounded-t-[50px]"}
+        max-h-[80%]
+        shadow-[0_-2px_10px_rgba(0,0,0,0.2)]
+        flex flex-col items-center
+      `}
+        // ${isCenter ? "min-h-0" : "min-h-[50%]"}
       >
         {!isCenter && (
           <div className="w-6 h-0.5 rounded-full bg-main-gray mb-2" />
