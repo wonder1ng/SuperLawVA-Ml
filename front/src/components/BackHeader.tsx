d86935ce3ddf0c41606266e3574f5fc69b957166
wonder1ng <wonder1ng@velog.io>
2025-06-13 00:30:18 +0900

    refactor: resize and modify position BackHeader

--- front/src/components/BackHeader.tsx
+++ front/src/components/BackHeader.tsx
@@ -6,20 +6,20 @@ interface BackLinkProps extends LinkIconProps {
 }
 
 export default function BackHeader({
-  width = 3,
-  height = 3,
+  width = 1.5,
+  height = 1.5,
   color = "#000000",
   to,
   children,
 }: BackLinkProps) {
   return (
-    <header className="grid items-end place-items-center relative">
+    <header className="grid items-center place-items-center relative w-full">
       <ArrowLeftIcon
         width={width}
         height={height}
         color={color}
         to={to}
-        className="col-start-1 row-start-1 relative -left-[16.8rem]"
+        className="col-start-1 row-start-1 justify-self-start ml-12"
       />
       {children && (
         <span className="col-start-1 row-start-1 leading-[2.9rem] font-semibold text-[2.4rem]">
